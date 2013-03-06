072e90af3747f6e1de4c8c5163dabc766aecf948
mehdi bagvand <mehdiebagvand@yahoo.com>
2013-03-06 13:06:46 +0330

    add some method to MainActivity.java
    for example ondestroy()

--- Calendar/src/com/ap/calendar/MainActivity.java
+++ Calendar/src/com/ap/calendar/MainActivity.java
@@ -13,6 +13,18 @@ public class MainActivity extends Activity {
 	}
 
 	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
